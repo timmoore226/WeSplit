c83a4b4c349d11d41089dd49de1660eed9f8b722
Tim Moore <timmoore226@gmail.com>
2020-05-16 11:37:34 -0400

    Initial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Timothy Moore on 5/15/20.
+//  Copyright © 2020 Timothy Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, World!")
+            }
+            Section {
+                Text("Hello, mother!")
+                Text("Hello, father!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
